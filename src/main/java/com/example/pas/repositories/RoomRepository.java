c553fabe1a8a0aa641860d52c0dc841ef4fda338
geoni-98 <chlrjsdnl@naver.com>
2025-03-28 18:37:21 +0900

    Delete src directory

--- src/main/java/com/example/pas/repositories/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.pas.repositories;
-
-import com.example.pas.models.Room;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface RoomRepository extends MongoRepository<Room, String> {
-    Optional<Room> findByCode(String code);
-}
--- src/main/java/com/example/pas/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.pas.repositories;
-
-import com.example.pas.models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByEmail(String email);
-}
